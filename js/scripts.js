911516e9853ebf3576d5238f46976a84cd9cce71
chynna <chynnalew@yahoo.com>
2021-08-10 11:11:58 -0700

    add dark mode button, add p#walrusInfo highlight toggle
    Co-authored-by: Melissa Schatz-Miller <melissa.scmi@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -7,4 +7,18 @@ $(document).ready(function() {
     $("#walrusHidden").fadeIn();
     $("#walrusShowing").fadeOut();
   });
+
+  $("button#darkMode").click(function() {
+    $("body").removeClass();
+    $("body").addClass("darkBackground");
+  });
+
+  $("button#lightMode").click(function(){
+    $("body").removeClass();
+  });
+
+  $("p#walrusInfo").click(function(){
+    $("p#walrusInfo").toggleClass("highlight");
+  });
+
 });
